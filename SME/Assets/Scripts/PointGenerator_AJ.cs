bd97a098ae92beeba4c5adde56a9145e6a69687e
AldousCYJeon <alje8278@colorado.edu>
2022-04-22 13:52:13 -0600

    Added method that generates an Array of vector3 points
    for all points:
    x=t
    y=sin(x^2}
    z=0
    
    NOT: Has not been tested

---
+++ SME/Assets/Scripts/PointGenerator_AJ.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointGenerator_AJ : MonoBehaviour
+{
+    public static PointGenerator_AJ Instance;
+    [SerializeField] private float m_step;
+    [SerializeField] private float m_start;
+    [SerializeField] private float m_stop;
+    private List<Vector3> m_points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.m_points = new List<Vector3>();
+
+        PointGenerator_AJ.Instance = this;
+    }
+
+    public Vector3[] GetPoints()
+    {
+        this.m_points = new List<Vector3>();
+        float t = this.m_start;
+        while (t < this.m_stop)
+        {
+            var x = t;
+            var y = this.GetYFromEq(x);
+
+            var hold = new Vector3(x, y, 0f);
+
+            this.m_points.Add(hold);
+
+            t += this.m_step;
+        }
+        return this.m_points.ToArray();
+    }
+
+    public float GetYFromEq(float x)
+    {
+        var x_2 = x * x;
+        var y = Mathf.Sin(x_2);
+        return y;
+    }
+}
