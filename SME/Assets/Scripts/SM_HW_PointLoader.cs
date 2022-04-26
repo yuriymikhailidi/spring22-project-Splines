c301981e2376dfd37dc75b2c0d6c2e47a9f1aa2d
rollegocoaster <49416717+rollegocoaster@users.noreply.github.com>
2022-04-26 13:19:21 -0600

    Update SM_HW_PointLoader.cs

--- SME/Assets/Scripts/SM_HW_PointLoader.cs
+++ SME/Assets/Scripts/SM_HW_PointLoader.cs
@@ -16,7 +16,6 @@ namespace SplineMesh {
         [SerializeField] private Vector2 _ErrorMean;
         [SerializeField] private Vector2 _variance;
         [SerializeField] private Vector2 _stdDev;
-        private int MaybeDont = 0;
 
         void Start()
         {
@@ -79,7 +78,7 @@ namespace SplineMesh {
                 
             }
             Debug.Log("the while loop is done");
-            this._ErrorMean = Vector2(totalError / n, totalRelError / n);
+            this._ErrorMean = new Vector2(totalError / n, totalRelError / n);
 
             //calc variance
             
@@ -105,69 +104,7 @@ namespace SplineMesh {
         // Update is called once per frame
         void Update()
         {
-            if(MaybeDont%600==601){// only call this ~ never every 10 seconds
-                Vector3[] Points = PointGenerator_AJ.Instance.GetPointList();
-                var values = PointGenerator_AJ.Instance.GetStepVal();
-
-                var step = values.x;
-                var start = values.y;
-                var stop = values.z;
-
-                var tDiff = stop - start;
-                var totalError = 0f;
-                var t = start;
-                var n = 0;
-                var numNodes = Points.Length;
-                Debug.Log("the while loop has started");
-                Debug.Log(numNodes);
-                List<float> splinevals = new List<float>();
-                List<float> splineErrors = new List<float>();
-                while (t < stop)
-                {
-                    var normT = (t - start) / tDiff;
-                    var Time = normT*numNodes;
-                    if(Time > numNodes-1){
-                        Debug.Log("Time was set to"+Time);
-                        Time = numNodes-1;
-                    }
-                    var other_version_of_Time =  Time - Mathf.FloorToInt(Time);
-
-                    var splineVal = this._Base_Spline.GetCurve(Time).GetLocation(other_version_of_Time).y;
-                    splinevals.Add(splineVal);
-                    
-                    var errorVal = PointGenerator_AJ.Instance.GetSplineError(splineVal, t);
-                    splineErrors.Add(errorVal);
-                    totalError += errorVal;
-
-                    n++;
-                    if(this._calcStep == 0){
-                        this._calcStep = 0.01f;
-                    }
-                    t += this._calcStep;
-                    
-                }
-                Debug.Log("the while loop is done");
-                this._errorMean = totalError / n;
-
-                //calc variance
-                
-                var totalVar = 0f;
-                t = start;
-                int n2 = 0;
-                while (t < stop)
-                {
-                    var diff = (splineErrors[n2] - this._errorMean);
-
-                    totalVar += diff * diff;
-
-                    t += this._calcStep;
-                    n2++;
-                }
-
-                this._variance = totalVar / n;
-                this._stdDev = Mathf.Sqrt(this._variance);
-            }
-            MaybeDont++;
+            
         }
 
         public float getY_prime(float x){
